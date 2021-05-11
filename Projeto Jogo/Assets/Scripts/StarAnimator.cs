7ea6506a838e06c83714635b6c87b67d85becec3
Daniel Calonico <calonicodaniel@gmail.com>
2021-05-10 21:38:00 -0300

    stars animation added

---
+++ Projeto Jogo/Assets/Scripts/StarAnimator.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarAnimator : MonoBehaviour
+    {
+
+    private Animator animator;
+    private AnimatorOverrideController animatorOverrider;
+    private bool dead;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        animator.SetTrigger("birth");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator Create()
+    {
+        if (dead)
+        {
+            dead = false;
+            animator.SetTrigger("birth");
+
+            yield return new WaitForSeconds(Random.Range(2, 20));
+
+            animator.SetTrigger("death");
+            dead = true;
+        }
+    }
+}
---
+++ Projeto Jogo/Assets/Scripts/StarAnimatorController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarAnimatorController : MonoBehaviour
+{
+
+    private GameObject[] starpool;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //starpool = new GameObject();
+        //for (int i = 0; i < 20; i++)
+        //{
+        //    starpool.add(transform.GetChild(i));
+        //}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
